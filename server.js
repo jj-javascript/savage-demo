8a2d689c8acbd13d6cecaaaf5f4dd86aab627b42
glitchwhostolexmas <justin.raf.jimenez@gmail.com>
2025-10-18 19:16:57 -0400

    first commit

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// Worked on this in a group setting led by Joshi
+
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -9,29 +11,29 @@ const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites
 const dbName = "demo";
 
 app.listen(3000, () => {
-    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-        if(error) {
-            throw error;
-        }
-        db = client.db(dbName);
-        console.log("Connected to `" + dbName + "`!");
-    });
+  MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+      throw error;
+    }
+    db = client.db(dbName);
+    console.log("Connected to `" + dbName + "`!");
+  });
 });
 
 app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   db.collection('messages').find().toArray((err, result) => {
     if (err) return console.log(err)
-    res.render('index.ejs', {messages: result})
+    res.render('index.ejs', { messages: result })
   })
 })
 
 app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+  db.collection('messages').insertOne({ name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown: 0 }, (err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
     res.redirect('/')
@@ -39,22 +41,30 @@ app.post('/messages', (req, res) => {
 })
 
 app.put('/messages', (req, res) => {
+  let thumbLogic
+  if (Object.keys(req.body)[2] == 'thumbUp') {
+    thumbLogic = req.body.thumbUp + 1
+    // Create conditional to toggle between put for thumbUp and thumbDown
+  } else if (Object.keys(req.body)[2] == 'thumbDown'){
+    thumbLogic = req.body.thumbDown - 1
+  }
+  console.log(thumbLogic)
   db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-    $set: {
-      thumbUp:req.body.thumbUp + 1
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
+    .findOneAndUpdate({ name: req.body.name, msg: req.body.msg }, {
+      $set: {
+        thumbUp: thumbLogic
+      }
+    }, {
+      sort: { _id: -1 },
+      upsert: true
+    }, (err, result) => {
+      if (err) return res.send(err)
+      res.send(result)
+    })
 })
 
 app.delete('/messages', (req, res) => {
-  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+  db.collection('messages').findOneAndDelete({ name: req.body.name, msg: req.body.msg }, (err, result) => {
     if (err) return res.send(500, err)
     res.send('Message deleted!')
   })
